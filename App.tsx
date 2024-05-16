a7fb7e7e4f078bf1c33698ef90142fcfa6fa4a2f
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-16 16:14:43 +0530

    modified function implementation to demonstrate dynamism

--- App.tsx
+++ App.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import {Text, View, Button} from 'react-native';
 import FirstComponent from './components/FirstComponent.tsx';
 import SecondComponent from './components/SecondComponent.tsx';
+
 const App = (): React.JSX.Element => {
 
-const handlePress=(value:string)=>{
-console.warn(value)
+let number = 20;
+const handlePress=()=>{
+number= 100;
+console.warn(number);
 }
   return (
 
       <View>
-        <Text style={{fontSize: 25, color: "black"}}>Buttons Demo with onPress</Text>
-        <Button title="Press Here" onPress={handlePress} color="green"/>
-        <Button title="Press Here" onPress={()=>handlePress("Hi Y'all")} color="blue"/>
+        <Text style={{fontSize: 25, color: "black"}}>Buttons Demo with onPress {number}</Text>
+        <Button title="Press Here" onPress={()=>handlePress()} color="green"/>
+        <Button title="Press Here" onPress={()=>handlePress()} color="blue"/>
       </View>
 
   );
