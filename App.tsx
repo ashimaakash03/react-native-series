c6a300cedfa3c46a076e37554a19da051b3ad1a5
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-16 15:50:26 +0530

    demonstrated buttons and handling onPress event

--- App.tsx
+++ App.tsx
@@ -3,22 +3,19 @@ import {Text, View, Button} from 'react-native';
 import FirstComponent from './components/FirstComponent.tsx';
 import SecondComponent from './components/SecondComponent.tsx';
 const App = (): React.JSX.Element => {
+
+const handlePress=(value:string)=>{
+console.warn(value)
+}
   return (
 
       <View>
-        <Text>Components</Text>
-        <FirstComponent/>
-        <SecondComponent/>
-        <FirstComponent/>
-        <SecondComponent/>
-        <FirstComponent/>
-        <SecondComponent/>
+        <Text style={{fontSize: 25, color: "black"}}>Buttons Demo with onPress</Text>
+        <Button title="Press Here" onPress={handlePress} color="green"/>
+        <Button title="Press Here" onPress={()=>handlePress("Hi Y'all")} color="blue"/>
       </View>
 
   );
 };
-
-
-
 
 export default App;
