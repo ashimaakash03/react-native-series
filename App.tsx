641d9a7f22de0654a3b2f0126d3b56916b5fc48f
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-16 14:16:54 +0530

    modified text, view and added button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import {Text, View} from 'react-native';
+import React from 'react';
+import {Text, View, Button} from 'react-native';
 
-const App = () => {
+const App = (): React.JSX.Element => {
   return (
-    <View>
-      <Text style={{fontSize: 30}}>Hello React-Native</Text>
-    </View>
+
+      <View>
+        <Text style={{fontSize: 20}}> Hello React Native <Text style={{fontSize: 30}}> Myself Ashim Aakash Roy </Text></Text>
+        <Button title='Press Here' color="red"/>
+      </View>
+
   );
 };
 
