277ac78d1dacea023fbec8f599fcce7cdf44e299
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-16 17:09:01 +0530

    demonstrated state and useState hook

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, Button} from 'react-native';
 import FirstComponent from './components/FirstComponent.tsx';
 import SecondComponent from './components/SecondComponent.tsx';
 
 const App = (): React.JSX.Element => {
+const [name, setName]= useState("Aakash");
+let data= "Ashim";
 
-let number = 20;
-const handlePress=()=>{
-number= 100;
-console.warn(number);
+const testUpdate = ()=>{
+    setName("Rishabh");
+    console.warn(name);
 }
+
   return (
 
       <View>
-        <Text style={{fontSize: 25, color: "black"}}>Buttons Demo with onPress {number}</Text>
-        <Button title="Press Here" onPress={()=>handlePress()} color="green"/>
-        <Button title="Press Here" onPress={()=>handlePress()} color="blue"/>
+        <Text style={{fontSize: 25, color: "black"}}>States in React Native</Text>
+        <Text style={{fontSize: 25, color: "red"}}>{name}</Text>
+        <Button onPress={testUpdate} title="Update Name"/>
+        <Text style={{fontSize: 25, color: "blue"}}>{data}</Text>
       </View>
 
   );
