1a05cc767a02d764745a79342da4bf2abffa5c8b
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-17 11:05:01 +0530

    demonstrated internal styling

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+
+const styles= StyleSheet.create({
+    textbox:{
+        fontSize: 30,
+        backgroundColor: "red",
+        color: "#fff",
+        margin: 20,
+        textAlign: "center",
+        textAlignVertical: "center",
+        borderColor: "yellow",
+        borderWidth: 5,
+        borderRadius: 20,
+        padding: 10
+    }
+})
 
 const App = (): React.JSX.Element => {
   return (
        <>
 	  <View>
+		<Text style= {styles.textbox}>Styling in React Native</Text>
 		<Text style= {{fontSize: 50, backgroundColor: "blue", color: "red"}}>Styling in React Native</Text>
-
-				<Text style= {{fontSize: 50, backgroundColor: "blue", color: "red"}}>Styling in React Native</Text>
+        <Text style= {styles.textbox}>Styling in React Native</Text>
+        <Text style= {{fontSize: 50, backgroundColor: "blue", color: "red"}}>Styling in React Native</Text>
+		<Text style= {styles.textbox}>Styling in React Native</Text>
 
 	  </View>
         </>
