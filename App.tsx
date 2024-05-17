70255d037165e7f76e9e243b3634fa7641cd5b36
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-17 10:49:15 +0530

    demonstrated inline styling

--- App.tsx
+++ App.tsx
@@ -1,23 +1,17 @@
-import React, {useState} from 'react';
-import {Text, View, Button} from 'react-native';
+import React from 'react';
+import {Text, View} from 'react-native';
 
 const App = (): React.JSX.Element => {
-    const [name, setName]= useState("");
   return (
        <>
 	  <View>
-		<Text style={{fontSize: 25, color: "black"}}>Props in React Native {name}</Text>
-		<Child name={name} age={30}/>
-		<Button title="Update Name" onPress={()=>setName("Aayush")}/>
+		<Text style= {{fontSize: 50, backgroundColor: "blue", color: "red"}}>Styling in React Native</Text>
+
+				<Text style= {{fontSize: 50, backgroundColor: "blue", color: "red"}}>Styling in React Native</Text>
+
 	  </View>
         </>
   );
 };
-
-const Child = ({name, age})=>{
-return <View>
-    <Text style={{color: "black", fontSize: 20}}>This is a child component inside App Component {name}. Your age is: {age}</Text>
-</View>
-}
 
 export default App;
