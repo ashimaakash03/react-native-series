8f4f897b1be443611f6e3a0c7a8d7320bd8bde7b
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-17 11:25:02 +0530

    applied external styling on App.tsx

--- App.tsx
+++ App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-
-const styles= StyleSheet.create({
-    textbox:{
-        fontSize: 30,
-        backgroundColor: "red",
-        color: "#fff",
-        margin: 20,
-        textAlign: "center",
-        textAlignVertical: "center",
-        borderColor: "yellow",
-        borderWidth: 5,
-        borderRadius: 20,
-        padding: 10
-    }
-})
+import ExtStyles from './style.js';
 
 const App = (): React.JSX.Element => {
   return (
        <>
 	  <View>
-		<Text style= {styles.textbox}>Styling in React Native</Text>
-		<Text style= {{fontSize: 50, backgroundColor: "blue", color: "red"}}>Styling in React Native</Text>
-        <Text style= {styles.textbox}>Styling in React Native</Text>
-        <Text style= {{fontSize: 50, backgroundColor: "blue", color: "red"}}>Styling in React Native</Text>
-		<Text style= {styles.textbox}>Styling in React Native</Text>
+		<Text style= {ExtStyles.textbox}>Styling in React Native</Text>
+		<Text style= {{fontSize: 50, backgroundColor: "blue", color: "red", textAlignVertical: "center", textAlign:"center", margin: 20, borderRadius: 10}}>Styling in React Native</Text>
+        <Text style= {ExtStyles.textbox}>Styling in React Native</Text>
+        <Text style= {{fontSize: 50, backgroundColor: "blue", color: "red", textAlignVertical: "center", textAlign:"center", margin: 20, borderRadius: 10}}>Styling in React Native</Text>
+		<Text style= {ExtStyles.textbox}>Styling in React Native</Text>
 
 	  </View>
         </>
---
+++ style.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export default ExtStyles= StyleSheet.create({
+    textbox:{
+        fontSize: 30,
+        backgroundColor: "blue",
+        color: "#fff",
+        margin: 20,
+        textAlign: "center",
+        textAlignVertical: "center",
+        borderColor: "orangered",
+        borderWidth: 5,
+        borderRadius: 20,
+        padding: 10
+    }
+});
