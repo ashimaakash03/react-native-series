eee41b85d930968d00d49a53329a19c28061af11
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-17 16:06:25 +0530

    demonstrated displaying list using map function

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 /* eslint-disable prettier/prettier */
 import React, {useState} from 'react';
-import { Text, View, StyleSheet, TextInput, Button, FlatList } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button, FlatList, ScrollView } from 'react-native';
 
 const App = (): React.JSX.Element => {
 
   const users= [
-  {name: 'Peter', age: 26, id:1},
-  {name: 'Tony', age: 37, id:2},
-  {name: 'Steve', age: 117, id:3},
-  {name: 'Clint', age: 29, id:4}
+  {name: 'Peter Parker', age: 26, id:1},
+  {name: 'Tony Stark', age: 37, id:2},
+  {name: 'Steve Rogers', age: 117, id:3},
+  {name: 'Clint Barton', age: 29, id:4},
+  {name: 'Thor', age: 1500, id:5},
+  {name: 'Bruce Banner', age: 40, id:6},
+  {name: 'Clarke Kent', age: 36, id:7},
+  {name: 'Bruce Banner', age: 37, id:8},
+  {name: 'Barry Allen', age: 17, id:9},
+  {name: 'Arthur Curry', age: 39, id:10},
+  {name: 'Oliver Queen', age: 35, id:11},
+  {name: 'Slade Wilson', age: 40, id:12},
+  {name: 'Martian Manhunter', age: 2000, id:13}
   ]
 
   return (
@@ -22,9 +31,11 @@ const App = (): React.JSX.Element => {
             backgroundColor: '#00f',
             padding: 20,
           }}>
-          Lists in React Native using FlatList
+          Lists in React Native using Map Function
         </Text>
-        <FlatList data={users} renderItem={({item})=><Text style={styles.listItems}>Name: {item.name} Age: {item.age}</Text>}/>
+        <ScrollView>
+        {users.map((item)=> <Text key={item.id} style={styles.listItems}>{item.name} {item.age}</Text>)}
+        </ScrollView>
       </View>
     </>
   );
