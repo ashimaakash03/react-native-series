4c43856eaf5ecec630ba5d15b25388985a075b83
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-17 15:39:10 +0530

    demonstrated displaying list using FlatList component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 /* eslint-disable prettier/prettier */
 import React, {useState} from 'react';
-import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button, FlatList } from 'react-native';
 
 const App = (): React.JSX.Element => {
 
-const resetForm= () =>{
-    setUserData(false);
-    setUsername('');
-    setEmail('');
-    setPassword('');
-}
-
-const [username, setUsername]= useState('');
-const [email, setEmail]= useState('');
-const [password, setPassword]= useState('');
-const [userdata, setUserData]= useState(false)
+  const users= [
+  {name: 'Peter', age: 26, id:1},
+  {name: 'Tony', age: 37, id:2},
+  {name: 'Steve', age: 117, id:3},
+  {name: 'Clint', age: 29, id:4}
+  ]
 
   return (
     <>
@@ -27,33 +22,27 @@ const [userdata, setUserData]= useState(false)
             backgroundColor: '#00f',
             padding: 20,
           }}>
-          Simple Form in React Native
+          Lists in React Native using FlatList
         </Text>
-        <TextInput placeholder='Enter Username' style={styles.textInput} onChangeText={(text)=> setUsername(text)} value={username}/>
-        <TextInput placeholder='Enter E-Mail' style={styles.textInput} onChangeText={(text)=> setEmail(text)} value={email}/>
-        <TextInput placeholder='Enter Password' style={styles.textInput} secureTextEntry={true} onChangeText={(text)=> setPassword(text)} value={password}/>
-        <View style={{margin: 10}}>
-        <Button title='show data' color='green' onPress={()=> setUserData(true)}/>
-        </View>
-        <View style={{margin: 10}}>
-        <Button title='clear data' color='red' onPress={()=> resetForm()}/>
-        </View>
-        <View style=
-        {{fontSize: 30, margin: 20, color:'blue'}}>
-        {userdata ? <View>
-        <Text> {username} </Text>
-        <Text>{email}</Text>
-        <Text>{password}</Text>
-        <Text>{userdata}</Text>
-        </View>: null}
-        </View>
+        <FlatList data={users} renderItem={({item})=><Text style={styles.listItems}>Name: {item.name} Age: {item.age}</Text>}/>
       </View>
     </>
   );
 };
 
 const styles= StyleSheet.create({
-    textInput:{fontSize:25, backgroundColor: 'whitesmoke', margin: 20, borderColor: 'blue', borderWidth: 2, borderRadius: 10, textAlignVertical: 'center'}
+    listItems: {
+        backgroundColor: 'yellow',
+        color: 'purple',
+        margin: 15,
+        padding: 10,
+        borderWidth: 2,
+        borderColor: '#a6a6a6',
+        borderRadius: 10,
+        height: 80,
+        fontSize: 25,
+        textAlign: 'center',
+        textAlignVertical: 'center'
+    }
 })
-
 export default App;
