659cad26bb4f303c75e2b2c9237dcd3713824aff
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-18 11:54:35 +0530

    modified Student component rendered Student component inside App component

--- components/Student.tsx
+++ components/Student.tsx
@@ -1,22 +1,16 @@
 import React, {useState, Component} from 'react';
 import { Text, View, StyleSheet, TextInput, Button, FlatList, ScrollView,  } from 'react-native';
-import Student from './components/Student.tsx'
 
-class App extends Component{
+class Student extends Component{
     render(){
         return(
             <View>
                 <Text style={{color:'#f00', backgroundColor:'#00f', fontSize:30}}>
-                    Class components in React Native
+                    Student component
                 </Text>
-                <View style={{margin:20}}>
-                <TextInput placeholder='Enter Name'/>
-                <Button title='Press here'/>
-                </View>
-                <Student/>
             </View>
         )
     }
 }
 
-export default App;
+export default Student;
