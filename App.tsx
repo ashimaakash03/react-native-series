2bdb2e368847099ece41dfe7b21608d2b6c94437
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-18 12:21:06 +0530

    demonstrated states and updating states in class components

--- App.tsx
+++ App.tsx
@@ -3,18 +3,27 @@ import { Text, View, StyleSheet, TextInput, Button, FlatList, ScrollView,  } fro
 import Student from './components/Student.tsx'
 
 class App extends Component{
-    handlePress(){
-        console.warn("Button Pressed...")
+
+    constructor(){
+        super();
+        this.state= {
+            name: 'Ashim',
+            age: 26
+        }
+    }
+
+    updateName(value){
+        this.setState({name: value});
     }
     render(){
         return(
             <View>
                 <Text style={{color:'#f00', backgroundColor:'#00f', fontSize:30}}>
-                    Class components in React Native
+                    {this.state.name} {this.state.age}
                 </Text>
                 <View style={{margin:20}}>
-                <TextInput placeholder='Enter Name'/>
-                <Button title='Press here' onPress={this.handlePress}/>
+                <TextInput placeholder='Enter Name' onChangeText={(text)=> this.updateName(text)}/>
+                <Button title='Press here'/>
                 </View>
                 <Student/>
             </View>
