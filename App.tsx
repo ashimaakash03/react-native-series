beee6a0a9029d908a42826ca671ec00f1069df2a
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 16:54:52 +0530

    implemented Activity Indicator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 const App = (): React.JSX.Element =>{
 
+const [activity, setActivity]= useState(false)
+const displayActivity=() =>{
+	setActivity(true);
+
+	setTimeout(()=>{
+		setActivity(false);
+	},3000)
+}
 	return(
 		<View style={styles.main}>
-
+ 			<Button title='Activity Toggler' onPress={displayActivity}/>
+ 			<ActivityIndicator size={100} color={"blue"} animating={activity}/>
 		</View>
 	)
 }
