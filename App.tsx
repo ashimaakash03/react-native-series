2b78bec68e5cbb62569afe9083565278ecd67d31
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 17:01:11 +0530

    reset UI

--- App.tsx
+++ App.tsx
@@ -3,18 +3,9 @@ import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, A
 
 const App = (): React.JSX.Element =>{
 
-const [activity, setActivity]= useState(false)
-const displayActivity=() =>{
-	setActivity(true);
-
-	setTimeout(()=>{
-		setActivity(false);
-	},3000)
-}
 	return(
 		<View style={styles.main}>
- 			<Button title='Activity Toggler' onPress={displayActivity}/>
- 			<ActivityIndicator size={100} color={"blue"} animating={activity}/>
+
 		</View>
 	)
 }
