a8fe7958aec1f9d06d213dff482edb9e89534ce6
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 12:07:20 +0530

    changed flex direction from default column to row

--- App.tsx
+++ App.tsx
@@ -4,18 +4,15 @@ import { Text, View, Button, StyleSheet } from 'react-native';
 const App = (): React.JSX.Element =>{
 	return(
 		<View style={styles.parent}>{/**Parent Container */}
-			<View style={styles.children}>
             			<View style={styles.child1}></View>{/**3/5 ratio */}
             			<View style={styles.child2}></View>
             			<View style={styles.child3}></View>
-            </View>
-		</View>
+        </View>
 	)
 }
 
 const styles= StyleSheet.create({
-	parent: {flex:1},
-	children: {flex:1},
+	parent: {flex:1, flexDirection: 'row'},
 	child1: {flex:3, backgroundColor:'red'},
 	child2: {flex:1, backgroundColor:'green'},
 	child3: {flex:1, backgroundColor:'blue'}
