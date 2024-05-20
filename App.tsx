115940433d5e6b521e0a718cda62bbd21f664437
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 12:46:24 +0530

    reset UI

--- App.tsx
+++ App.tsx
@@ -1,28 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 const App = (): React.JSX.Element =>{
 	return(
-		<View style={styles.parent}>{/**Parent Container */}
-            			<View style={styles.child1}>
-            			    <View style={styles.child1ofred}><Text>Purple box</Text></View>
-            			    <View style={styles.child2ofred}><Text>Yellow box</Text></View>
-            			    <View style={styles.child3ofred}><Text>YellowGreen Box</Text></View>
-            			</View>{/**3/5 ratio */}
-            			<View style={styles.child2}></View>
-            			<View style={styles.child3}></View>
+		<View>
+            <Text style={{color:'#f00', backgroundColor:'#00f', fontSize:30, marginBottom:30, padding:10, textAlign: 'center'}}>
+        				TouchableHighlight
+            </Text>
         </View>
 	)
 }
-
-const styles= StyleSheet.create({
-	parent: {flex:1, flexDirection: 'column'},
-	child1: {flex:2, backgroundColor:'red', flexDirection:'row'},
-	child1ofred: {flex:2, backgroundColor:'purple', margin:10, padding:10},
-	child2ofred: {flex:1, backgroundColor:'yellow', margin:10, padding:10},
-	child3ofred: {flex:1, backgroundColor:'yellowgreen', margin:10, padding:10},
-	child2: {flex:1, backgroundColor:'green'},
-	child3: {flex:1, backgroundColor:'blue'}
-})
 
 export default App;
