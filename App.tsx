abe7844b90fd8fe3bcd9c6d84ed1294abcab33d5
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 11:54:18 +0530

    demonstrated area occupancy using flexbox

--- App.tsx
+++ App.tsx
@@ -2,12 +2,20 @@ import React, {useState, useEffect} from 'react';
 import { Text, View, Button } from 'react-native';
 
 const App = (): React.JSX.Element =>{
-	const [display, setDisplay]= useState(true);
 	return(
-		<View>
-			<Text style={{color:'#f00', backgroundColor:'#00f', fontSize:30, marginBottom:30, padding:10, textAlign: 'center'}}>
-				Responsive UI with flex
-			</Text>
+		<View style={{flex:1}}>{/**Parent Container */}
+			<View style={{flex:1}}>
+            			<View style={{flex:3, backgroundColor:'red'}}></View>{/**3/5 ratio */}
+            			<View style={{flex:1, backgroundColor:'green'}}></View>
+            			<View style={{flex:1, backgroundColor:'blue'}}></View>
+            </View>
+            <View style={{flex:1}}>
+				<View style={{flex:1}}>
+                    <View style={{flex:3, backgroundColor:'red'}}></View>
+                    <View style={{flex:3, backgroundColor:'green'}}></View>
+                    <View style={{flex:4, backgroundColor:'blue'}}></View>{/**4/10 ratio */}
+                </View>
+           	</View>
 		</View>
 	)
 }
