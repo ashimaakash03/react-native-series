7976920c12f8f1b85e9a44c7063d64cf5b506798
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 17:57:25 +0530

    implemented displaying message through modal and open/close buttons functionality

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator, Modal } from 'react-native';
 
 const App = (): React.JSX.Element =>{
+
+const [showModal, setShowModal]= useState(false);
 
 	return(
 		<View style={styles.main}>
 
+			<View style={styles.wrapper}>
+				<Button title='Open Modal' onPress={()=> setShowModal(!showModal)}/>
+				{showModal?<Modal transparent={true} visible={showModal} animationType={"slide"}>
+
+                           				<View style={styles.modalContainer}>
+                           					<View style={styles.modalBox}>
+                           						<Text style={styles.modalText}> Hi!! This This a modal...</Text>
+                           						<Button title='Close Modal' onPress={()=>setShowModal(!showModal)}/>
+                           					</View>
+                           				</View>
+
+                           			</Modal>: null}
+			</View>
 		</View>
 	)
 }
@@ -13,8 +28,26 @@ const App = (): React.JSX.Element =>{
 const styles= StyleSheet.create({
 	main:{
 		flex:1,
-		alignItems:'center',
-		justifyContent: 'center'
+	},
+	wrapper:{
+		flex:1,
+		justifyContent: 'flex-end'
+	},
+	modalContainer:{
+		flex:1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	modalBox:{
+		backgroundColor: 'whitesmoke',
+        padding: 40,
+        borderRadius: 20,
+        borderWidth: 1,
+
+	},
+	modalText:{
+		fontSize: 20,
+		color: '#bfbfbf'
 	}
 })
 
