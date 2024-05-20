898e048b90247c2f744bd29ca2e13c86bf55a642
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 16:14:14 +0530

    reset UI

--- App.tsx
+++ App.tsx
@@ -3,18 +3,9 @@ import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity } 
 
 const App = (): React.JSX.Element =>{
 
-const [selectedRadio, setSelectedRadio]= useState(0);
-const skills= [{id: 1, name:"JAVA"}, {id:2, name:"HTML5"}, {id:3, name:"CSS3"}, {id:4, name:"JS & AJAX"}, {id:5, name: "React"}, {id:6, name:"React Native"}];
 	return(
 		<View style={styles.main}>
-			{skills.map((item)=><TouchableOpacity key={item.id} onPress={()=> setSelectedRadio(item.id)}>
-                            				<View style={styles.wrapper}>
-                            					<View style={styles.radio}>
-                            						{selectedRadio=== item.id? <View style={styles.background}></View>: null}
-                            					</View>
-                            					<Text style={styles.radioText}>{item.name}</Text>
-                            				</View>
-                            			</TouchableOpacity>)}
+
 		</View>
 	)
 }
@@ -24,29 +15,6 @@ const styles= StyleSheet.create({
 		flex:1,
 		alignItems:'center',
 		justifyContent: 'center'
-	},
-	radioText:{
-		fontSize: 20,
-		color: 'black',
-	},
-	radio:{
-		height:40,
-		width:40,
-		borderWidth: 3,
-		borderColor: 'black',
-		borderRadius: 20,
-		margin: 10
-	},
-	wrapper:{
-		flexDirection: 'row',
-		alignItems: 'center'
-	},
-	background:{
-		backgroundColor: 'black',
-		height: 28,
-		width: 28,
-		borderRadius: 20,
-		margin:3
 	}
 })
 
