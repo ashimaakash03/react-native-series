677220203261b81d1df8a8ead7cb24a92b1469b1
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 11:19:43 +0530

    reset UI

--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import { Text, View, Button } from 'react-native';
-import Child from './components/Child.tsx';
 
 const App = (): React.JSX.Element =>{
 	const [display, setDisplay]= useState(true);
 	return(
 		<View>
 			<Text style={{color:'#f00', backgroundColor:'#00f', fontSize:30, marginBottom:30, padding:10, textAlign: 'center'}}>
-				Show / Hide components using useEffect
+				Responsive UI with flex
 			</Text>
-			<Button title='Toggle' onPress={()=>setDisplay(!display)}/>
-			{display? <View style={{color:'purple', backgroundColor:'yellow', marginTop: 30}}><Child/></View>: null}
-			</View>
+		</View>
 	)
 }
 
