1cbc3ef5cd2f75bacdfa844d34bb5e3fe8d0e142
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 14:45:02 +0530

    reset UI

--- App.tsx
+++ App.tsx
@@ -4,44 +4,8 @@ import { Text, View, Button, TouchableHighlight, StyleSheet } from 'react-native
 const App = (): React.JSX.Element =>{
 	return(
 		<View>
-            <View style={styles.main}>
-                <TouchableHighlight>
-                    <Text style={[styles.button, styles.primary]}>Primary Button</Text>
-                </TouchableHighlight>
-                <TouchableHighlight>
-                    <Text style={[styles.button, styles.success]}>Success Button</Text>
-                </TouchableHighlight>
-                <TouchableHighlight>
-                    <Text style={[styles.button, styles.warning]}>Warning Button</Text>
-                </TouchableHighlight>
-                <TouchableHighlight>
-                    <Text style={[styles.button, styles.error]}>Error Button</Text>
-                </TouchableHighlight>
-                <TouchableHighlight>
-                    <Text style={styles.button}>Button</Text>
-                </TouchableHighlight>
-            </View>
-        </View>
+		</View>
 	)
 }
-
-const styles= StyleSheet.create({
-	main:{
-	},
-	button:{
-		backgroundColor: 'grey',
-		color: 'white',
-		margin:10,
-		padding: 10,
-		textAlign: 'center',
-		textTransform: 'uppercase',
-		borderRadius: 10,
-		elevation: 10
-	},
-	primary:{backgroundColor: 'blue'},
-	success:{backgroundColor: 'green'},
-	warning:{backgroundColor: 'gold'},
-	error: {backgroundColor: 'red'}
-})
 
 export default App;
