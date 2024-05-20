45690330f4c215fcc00b909fa0fa6b6fdd15bca0
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-20 14:35:38 +0530

    created different kinds of buttons

--- App.tsx
+++ App.tsx
@@ -1,14 +1,47 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, TouchableHighlight, StyleSheet } from 'react-native';
 
 const App = (): React.JSX.Element =>{
 	return(
 		<View>
-            <Text style={{color:'#f00', backgroundColor:'#00f', fontSize:30, marginBottom:30, padding:10, textAlign: 'center'}}>
-        				TouchableHighlight
-            </Text>
+            <View style={styles.main}>
+                <TouchableHighlight>
+                    <Text style={[styles.button, styles.primary]}>Primary Button</Text>
+                </TouchableHighlight>
+                <TouchableHighlight>
+                    <Text style={[styles.button, styles.success]}>Success Button</Text>
+                </TouchableHighlight>
+                <TouchableHighlight>
+                    <Text style={[styles.button, styles.warning]}>Warning Button</Text>
+                </TouchableHighlight>
+                <TouchableHighlight>
+                    <Text style={[styles.button, styles.error]}>Error Button</Text>
+                </TouchableHighlight>
+                <TouchableHighlight>
+                    <Text style={styles.button}>Button</Text>
+                </TouchableHighlight>
+            </View>
         </View>
 	)
 }
+
+const styles= StyleSheet.create({
+	main:{
+	},
+	button:{
+		backgroundColor: 'grey',
+		color: 'white',
+		margin:10,
+		padding: 10,
+		textAlign: 'center',
+		textTransform: 'uppercase',
+		borderRadius: 10,
+		elevation: 10
+	},
+	primary:{backgroundColor: 'blue'},
+	success:{backgroundColor: 'green'},
+	warning:{backgroundColor: 'gold'},
+	error: {backgroundColor: 'red'}
+})
 
 export default App;
