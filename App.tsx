524e03f2a1e2daba2272ed7123c92e49894f106f
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-22 11:15:07 +0530

    implemented Pressable button for all types of presses

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator, Modal } from 'react-native';
+import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator, Modal, Pressable } from 'react-native';
 
 const App = (): React.JSX.Element =>{
 
@@ -7,6 +7,13 @@ const [showModal, setShowModal]= useState(false);
 
 	return(
 		<View style={styles.main}>
+			<Pressable
+			onPress={()=>console.warn("Normal Press")}
+			onLongPress={()=> console.warn("Press and hold")}
+			onPressIn={()=> console.warn("Press in...")}
+			onPressOut={()=> console.warn("Press out...")}>
+				<Text style={styles.pressableBtn}>Pressable</Text>
+			</Pressable>
 		</View>
 	)
 }
@@ -14,6 +21,18 @@ const [showModal, setShowModal]= useState(false);
 const styles= StyleSheet.create({
 	main:{
 		flex:1,
+		justifyContent: 'center'
+	},
+	pressableBtn:{
+		backgroundColor:'blue',
+		color: '#fff',
+		margin:10,
+		padding: 10,
+		textAlign:'center',
+		fontSize:25,
+		borderRadius:10,
+		shadowColor: '#f00',
+		elevation:4
 	}
 })
 
