8f675c74ee2feef424820f3a2587d4b14bc05fd0
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-22 13:24:33 +0530

    reset UI

--- App.tsx
+++ App.tsx
@@ -4,9 +4,7 @@ import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, A
 const App = (): React.JSX.Element =>{
 	return(
 		<View style={styles.main}>
-			<Text style={{fontSize:30, color:'#fff', textAlign:'center', backgroundColor:'red', margin:20, padding:10}}>Platform: {Platform.OS}</Text>
-			{Platform.OS === 'android'? <View style={{backgroundColor:'red', height:100, width:100, margin:20, padding:10}}></View>: <View style={{backgroundColor:'green', height:100, width:100, margin:20, padding:10}}></View>}
-			<Text style={styles.text}>{JSON.stringify(Platform.constants.reactNativeVersion.minor)}</Text>
+
 		</View>
 	)
 }
@@ -16,10 +14,6 @@ const styles= StyleSheet.create({
 		flex:1,
 		alignItems:'center',
 		justifyContent: 'space-around'
-	},
-	text:{
-		fontSize:30,
-		color: Platform.OS === 'android' ? 'red' : 'green'
 	}
 })
 
