0e8b4f06eb4e8e94381fa637d408f843121d9a07
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-22 18:50:25 +0530

    reset UI

--- App.tsx
+++ App.tsx
@@ -2,10 +2,13 @@ import React, {useState, useEffect} from 'react';
 import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator, Modal, Pressable, Platform } from 'react-native';
 import {WebView} from 'react-native-webview';
 
+
 const App = (): React.JSX.Element =>{
 
 	return(
-		<WebView source={{uri: 'https://www.google.com'}}/>
+		<View style={styles.main}>
+			<Text style={styles.text}>Stack Navigation</Text>
+		</View>
 	)
 }
 
@@ -13,6 +16,14 @@ const styles= StyleSheet.create({
 	main:{
 		flex:1,
 		justifyContent: 'flex-start'
+	},
+	text:{
+		fontSize: 30,
+		color: '#fff',
+		backgroundColor: '#00f',
+		textAlign: 'center',
+		margin: 20,
+		padding: 10
 	}
 })
 
