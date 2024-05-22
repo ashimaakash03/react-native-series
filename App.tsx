301af0f9296f0d4c9059b743c7b184edd69b3b9d
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-22 12:46:42 +0530

    explored Platform feature

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator, Modal, Pressable } from 'react-native';
+import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator, Modal, Pressable, Platform } from 'react-native';
 
 const App = (): React.JSX.Element =>{
-
-const [showModal, setShowModal]= useState(false);
-
 	return(
 		<View style={styles.main}>
-
+			<Text style={{fontSize:30, color:'#fff', textAlign:'center', backgroundColor:'red', margin:20, padding:10}}>Platform: {Platform.OS}</Text>
+			{Platform.OS === 'android'? <View style={{backgroundColor:'red', height:100, width:100, margin:20, padding:10}}></View>: <View style={{backgroundColor:'green', height:100, width:100, margin:20, padding:10}}></View>}
+			<Text style={styles.text}>{JSON.stringify(Platform.constants.reactNativeVersion.minor)}</Text>
 		</View>
 	)
 }
@@ -15,6 +14,12 @@ const [showModal, setShowModal]= useState(false);
 const styles= StyleSheet.create({
 	main:{
 		flex:1,
+		alignItems:'center',
+		justifyContent: 'space-around'
+	},
+	text:{
+		fontSize:30,
+		color: Platform.OS === 'android' ? 'red' : 'green'
 	}
 })
 
