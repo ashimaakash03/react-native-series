6d82e2085c698db460425df725b8fe27e3c6ac6b
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-22 16:23:46 +0530

    installed required dependencies for navigation

--- App.tsx
+++ App.tsx
@@ -6,13 +6,7 @@ const[show, setShow]= useState(false)
 
 	return(
 		<View style={styles.main}>
-			{show ? <View style={styles.modal}>
-                    				<View style={styles.body}>
-                    					<Text>Dummy Random Text</Text>
-                    					<Button title='Close Dialog Box' onPress={() => setShow(false)}/>
-                    				</View>
-                    			</View>: null}
-			<Button title='Open Dialog Box' onPress={() => setShow(true)}/>
+
 		</View>
 	)
 }
@@ -21,20 +15,6 @@ const styles= StyleSheet.create({
 	main:{
 		flex:1,
 		justifyContent: 'flex-end'
-	},
-	modal:{
-		flex:1,
-		backgroundColor: 'rgba(0,0,0,0.3)',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	body:{
-		width: 200,
-		height: 200,
-		backgroundColor: 'white',
-		justifyContent: 'space-around',
-		alignItems: 'center',
-		borderRadius: 20
 	}
 })
 
