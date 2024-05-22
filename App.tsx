9c301ac278ed4b62d5c94978363ec63557e73048
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-22 14:34:00 +0530

    created custom modal and added button functionality

--- App.tsx
+++ App.tsx
@@ -2,9 +2,17 @@ import React, {useState, useEffect} from 'react';
 import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator, Modal, Pressable, Platform } from 'react-native';
 
 const App = (): React.JSX.Element =>{
+const[show, setShow]= useState(false)
+
 	return(
 		<View style={styles.main}>
-
+			{show ? <View style={styles.modal}>
+                    				<View style={styles.body}>
+                    					<Text>Dummy Random Text</Text>
+                    					<Button title='Close Dialog Box' onPress={() => setShow(false)}/>
+                    				</View>
+                    			</View>: null}
+			<Button title='Open Dialog Box' onPress={() => setShow(true)}/>
 		</View>
 	)
 }
@@ -12,8 +20,21 @@ const App = (): React.JSX.Element =>{
 const styles= StyleSheet.create({
 	main:{
 		flex:1,
-		alignItems:'center',
-		justifyContent: 'space-around'
+		justifyContent: 'flex-end'
+	},
+	modal:{
+		flex:1,
+		backgroundColor: 'rgba(0,0,0,0.3)',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	body:{
+		width: 200,
+		height: 200,
+		backgroundColor: 'white',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		borderRadius: 20
 	}
 })
 
