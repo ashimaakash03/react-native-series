72b523e73bbe388ffd5f1d7d40d15c2961a1aa3a
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-22 17:38:46 +0530

    implemented WebView to run other websites

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import { Text, View, Button, TouchableHighlight, StyleSheet, TouchableOpacity, ActivityIndicator, Modal, Pressable, Platform } from 'react-native';
+import {WebView} from 'react-native-webview';
 
 const App = (): React.JSX.Element =>{
-const[show, setShow]= useState(false)
 
 	return(
-		<View style={styles.main}>
-			<Text>Hi y'all</Text>
-		</View>
+		<WebView source={{uri: 'https://www.google.com'}}/>
 	)
 }
 
